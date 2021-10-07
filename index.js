35ac78347f5f1df1f1052ad9fa18c744a853b8b3
Raghavendra kumar <raghavendrakumar217@gmail.com>
2021-10-07 15:45:08 +0530

    express

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("hello world");
-console.log("firsj");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome  express</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Welcome to  page by GET Method</h1>");
+});
+
+app.post("/about", (req, res) => {
+  res.send("<h1>Welcome hello   page by POST method</h1>");
+});
+
+app.listen(3600, (req, res) => {
+  console.log("Server Running on 3500");
+});
---
+++ indexHTTP.js
@@ -0,0 +1,27 @@
+// const http = require("http");
+
+// http
+//   .createServer((req, res) => {
+//     // console.log(req);
+//     if (req.url == "/") {
+//       res.write("<h1>Welcome To Nodejs</h1>");
+//       res.end();
+//     } else if (req.url == "/contact") {
+//       res.write("<h1>Contact Us</h1>");
+//       res.end();
+//     } else if (req.url == "/contact/about") {
+//       res.write("<h1>About</h1>");
+//       res.end();
+//     }
+//   })
+//   .listen(3200);
+
+// const h = require("http");
+
+// h.createServer((req, res) => {
+//   //   console.log(req);
+//   if (req.url == "/") {
+//     res.write("<h1>I am in server 3400</h1>");
+//     res.end();
+//   }
+// }).listen(3400);
